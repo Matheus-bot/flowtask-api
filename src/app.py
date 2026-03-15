cca0a63fd69ca5365bd945bab155b1274015bbd9
Matheus <matheushenrry00@gmail.com>
2026-03-15 19:59:17 -0300

    Atualiza títuo da página inicial

--- src/app.py
+++ src/app.py
@@ -11,9 +11,9 @@ def status():
 def home():
     return "Sistema de Biblioteca Online"
 
-@app.route("/sobre")
-def sobre():
-    return "Sistema desenvolvido em Flask para estudo de CI/CD"
+@app.route("/")
+def home():
+    return "Sistema de Gestão de Biblioteca"
 @app.route("/livros")
 def livros():
     return "Lista de livros cadastrados"
